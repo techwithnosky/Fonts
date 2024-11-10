5d5f0273b23ce5eb1819b6cd16a69eddabc0ad7e
techwithnosky <techwithnosky@gmail.com>
2024-11-10 16:02:09 +0100

    Add files via upload

---
+++ formHandler.js
@@ -0,0 +1,29 @@
+// serverlessFunction.js
+const mailchimp = require('@mailchimp/mailchimp_marketing');
+
+mailchimp.setConfig({
+  apiKey: '72e9a4368ccf74841b6558b903b0f022-us13',
+  server: 'us13'
+});
+
+export async function handler(request, response) {
+  const { name, country } = request.body;
+
+  const audienceId = 'fab6eda132';
+
+  try {
+    const response = await mailchimp.lists.addListMember(audienceId, {
+      email_address: 'emmynosky424@gmail.com',
+      status: 'subscribed',
+      merge_fields: {
+        FNAME: name,
+        COUNTRY: country
+      }
+    });
+
+    response.status(200).json({ message: 'Form submitted successfully' });
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: 'Error submitting form' });
+  }
+}
